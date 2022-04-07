fe0964bc054f965e8370abaa48f7a8bff2581fda
Swetarun <er.shweta.singh990@gmail.com>
2022-04-07 21:32:09 +0530

    trial

--- src/routes/route.js
+++ src/routes/route.js
@@ -11,6 +11,25 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
+router.get('/all-candidates', function (req, res) {
+    const names = ['Sweta','Tarun','Taksh','Mithun','Swati','Soni','Ankit']
+    let arr = []
+    for (let i = 0; i<names.length; i++){
+        arr.push(names[i])
+    }
+    res.send(arr)
+});
+
+router.get('/candidates', function (req, res) {
+    const names = ['Sweta','Tarun','Taksh','Mithun','Swati','Soni','Ankit']
+    let arr = []
+    let n = req.query.count
+    for (let i = 0; i<n; i++){
+        arr.push(names[i])
+    }
+    res.send(arr)
+});
+
 
 
 
